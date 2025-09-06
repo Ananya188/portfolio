bbb6d91e5d887a9250b80bd6fc14a4cf7fbf129a
Ananya Dutta <60771644+Ananya188@users.noreply.github.com>
2025-09-06 19:47:01 +0530

    photo

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export const HeroSection = () => {
         <div className='container max-w-4xl mx-auto text-center z-10'>
             <div className="space-y-6">
                 <div className='h-[12rem] w-[12rem] rounded-full overflow-hidden mb-[2rem] mx-auto'>
-                <img src="public/from_fill.png" alt="myImage" />
+                <img src="./from_fill.png" alt="myImage" />
                 </div>
                 <h1 className='text-4xl md:text-6xl font-bold tracking-tight'>
                     <span className="opacity-0 animate-fade-in">Hi, I'm</span>
